cc57bb2d3b6517d25d0f05d9b4715b8397e74dd5
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-11-13 18:40:42 -0500

    Completed form

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,7 @@ function App() {
             placeholder: "Username",
             errorMessage: "Username should be 3-16 characters and shouldn't include any special character!",
             label: "Username",
+            pattern: "^[A-Za-x0-9]{3,16}",
             required: true,
         },
         {
@@ -55,6 +56,7 @@ function App() {
             placeholder: "Password",
             errorMessage: "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
             label: "Password",
+            pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
             required: true,
         },
         {
@@ -64,6 +66,7 @@ function App() {
             placeholder: "Confirm Password",
             errorMessage: "Passwords don't match!",
             label: "Confirm Password",
+            pattern: values.password,
             required: true,
         },
     ];
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import "./FormInput.css";
 
 
 function FormInput(props) {
+    const [focused, setFocused] = useState(false);
 
     const { label, errorMessage, handleChange, ...inputProps } = props;
+
+
+    const handleFocus = (e) => {
+        setFocused(true);
+    }
 
     return (
         <div className="formInput">
             <label htmlFor="">{label}</label>
-            <input {...inputProps} onChange={handleChange} />
+            <input {...inputProps} onChange={handleChange} onBlur={handleFocus} onFocus={inputProps.name === "confirmPassword" ? () => setFocused(true) : null} focused={focused.toString()} />
             <span>{errorMessage}</span>
         </div>
     );
